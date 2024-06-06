35673225169d5a721c9b4eb9de3905957fc8abd8
dzakwandaffar <dzakwandaffar@gmail.com>
2024-06-06 16:42:45 +0700

    next commit

--- handler/transaction.go
+++ handler/transaction.go
@@ -1,6 +1,8 @@
 package handler
 
 import (
+	"api_gateway/model"
+	"api_gateway/utils"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -20,14 +22,27 @@ type BodyPayloadTransaction struct{}
 
 func (b *transactionImplement) TransferBank(g *gin.Context) {
 
-	bodyPayloadTxn := BodyPayloadTransaction{}
+	bodyPayloadTxn := model.Transaction{}
 	err := g.BindJSON(&bodyPayloadTxn)
-
 	if err != nil {
 		g.AbortWithError(http.StatusBadRequest, err)
 	}
 
+	orm := utils.NewDatabase().Orm
+	db, _ := orm.DB()
+
+	defer db.Close()
+
+	result := orm.Create(&bodyPayloadTxn)
+	if result.Error != nil {
+		g.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+			"error": result.Error,
+		})
+		return
+	}
+
 	g.JSON(http.StatusOK, gin.H{
-		"message": "Hello guys this API rest for later",
+		"message": "Transaction Succesfull",
+		"data":    bodyPayloadTxn,
 	})
 }
