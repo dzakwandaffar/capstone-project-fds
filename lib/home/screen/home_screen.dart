4cd03de115e80f60763836fa7cb1be65d2f14024
syafiulhuda <msyafiulhuda1@gmail.com>
2024-06-14 23:33:20 +0700

    feat: first upload

---
+++ lib/home/screen/home_screen.dart
@@ -0,0 +1,56 @@
+import 'package:bank_celengan/home/screen_detail/account_screen_detail.dart';
+import 'package:bank_celengan/home/screen_detail/home_screen_detail.dart';
+import 'package:bank_celengan/home/screen_detail/mutasi_screen_detail.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  static Route<void> route() {
+    return MaterialPageRoute(
+      builder: (_) => const HomeScreen(),
+    );
+  }
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _currentIndex = 0;
+
+  final List<Widget> _pages = [
+    const HomeScreenDetail(),
+    const MutasiScreenDetail(),
+    const AccountScreenDetail(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _pages[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.receipt_long),
+            label: 'Mutasi',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_circle),
+            label: 'Account',
+          ),
+        ],
+      ),
+    );
+  }
+}
